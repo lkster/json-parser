37eeabbf21ba1feb53dec03b5bcf0fe4023c9616
Dominik Mielcarek <work@mielcarek.dev>
2024-03-03 23:26:55 +0100

    feat(dart): implemented NextParserExtension to call for next extension in chain

---
+++ dart/lib/core/parser/next_parser_extension.dart
@@ -0,0 +1,35 @@
+/// This is used to call next extension if actual one does not feel it can do something with currently provided token.
+/// The reason why it's note done in the same way Lexer is, is because parser extensions can nest things in case of
+/// complex structures. Eg. object parser would nest parsing structures within it and include them in itself
+///
+/// Next thing is why it provides new instance of itself to onNext call. It's to have always same index for current
+/// extension:
+///
+/// next() calls parse() method of next extension and returns its value
+///
+/// Extension1 calls next <- 0 index
+///   Extension2 calls next() <- 1 index
+///   Extension2 calls next() one more time <- still 1 index
+///     Extension3 calls next() <- 2 index
+/// Coming back to Extension1's next <- still 0 index
+///
+/// Otherwise during such recursion index would be increased for every extension in chain while we always need extension
+/// that is directly after current one
+///
+/// Actual usage in Parser
+///
+final class NextParserExtension {
+  final int index;
+
+  final Object Function(int index, NextParserExtension next) onNext;
+
+  NextParserExtension({this.index = 0, required this.onNext});
+
+  Object call() {
+    return onNext(index, NextParserExtension(index: index + 1, onNext: onNext));
+  }
+
+  Object startOver() {
+    return onNext(0, NextParserExtension(index: 1, onNext: onNext));
+  }
+}
---
+++ dart/test/core/parser/next_parser_extension_test.dart
@@ -0,0 +1,61 @@
+import 'package:json_parser/core/parser/next_parser_extension.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('should call onNext function', () {
+    var called = false;
+    final next = NextParserExtension(onNext: (index, next) {
+      called = true;
+
+      return 0;
+    });
+
+    next();
+
+    expect(called, true);
+  });
+
+  test('should not change its index value on next() call', () {
+    final next = NextParserExtension(onNext: (index, next) => 0);
+
+    next();
+    next();
+    next();
+
+    expect(next.index, 0);
+  });
+
+  test('should provide new NextParserExtension with increased index', () {
+    late NextParserExtension next;
+
+    next = NextParserExtension(onNext: (index, nextFunc) {
+      next = nextFunc;
+
+      return 0;
+    });
+
+    next();
+    next();
+    next();
+
+    expect(next.index, 3);
+  });
+
+  test('should reset index in returned NextParserExtension when startOver is called', () {
+    late NextParserExtension next;
+
+    next = NextParserExtension(onNext: (index, nextFunc) {
+      next = nextFunc;
+
+      return 0;
+    });
+
+    next();
+    next();
+    next();
+    next.startOver();
+
+    // onNext is instantly called with 0 index hence returned nextFunc has already increased value
+    expect(next.index, 1);
+  });
+}
