c9b7daee6a74e304553a1ec5a0f63889fdf06f42
Dominik Mielcarek <work@mielcarek.dev>
2024-03-04 02:07:31 +0100

    feat(dart.lexer): added DelimiterLexerExtension

---
+++ dart/lib/lexer_extensions/delimiter_lexer_extension.dart
@@ -0,0 +1,60 @@
+import 'package:json_parser/core/lexer/lexer_extension.dart';
+import 'package:json_parser/core/lexer/scanner.dart';
+import 'package:json_parser/core/lexer/token.dart';
+import 'package:json_parser/core/utils/char_code.dart';
+
+enum Delimiter {
+  comma(','),
+  colon(':'),
+  doubleQuote('"'),
+  openBracket('['),
+  closeBracket(']'),
+  openBrace('{'),
+  closeBrace('}');
+
+  const Delimiter(this.symbol);
+
+  final String symbol;
+}
+
+
+final class DelimiterToken extends Token {
+  final Delimiter value;
+
+  const DelimiterToken(this.value);
+
+  @override
+  int get hashCode => Object.hash(runtimeType, value);
+
+  @override
+  bool operator ==(Object other) => other is DelimiterToken && value == other.value;
+
+  @override
+  String toString() {
+    return value.symbol;
+  }
+}
+
+final class DelimiterLexerExtension implements LexerExtension {
+  final _delimiters = {
+    CharCode.openBrace.code: Delimiter.openBrace,
+    CharCode.closeBrace.code: Delimiter.closeBrace,
+    CharCode.openBracket.code: Delimiter.openBracket,
+    CharCode.closeBracket.code: Delimiter.closeBracket,
+    CharCode.colon.code: Delimiter.colon,
+    CharCode.comma.code: Delimiter.comma,
+    CharCode.doubleQuote.code: Delimiter.doubleQuote,
+  };
+
+  @override
+  Token? lex(Scanner scanner) {
+    if (!_delimiters.containsKey(scanner.peekNonWhitespace())) {
+      return null;
+    }
+
+    final delimiter = _delimiters[scanner.readChar()]!;
+
+    return DelimiterToken(delimiter);
+  }
+
+}
---
+++ dart/test/lexer_extensions/delimiter_lexer_extension_test.dart
@@ -0,0 +1,24 @@
+import 'package:json_parser/core/lexer/scanner.dart';
+import 'package:json_parser/lexer_extensions/delimiter_lexer_extension.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('should return DelimiterToken if delimiter is lexed', () {
+    var scanner = Scanner('{[":,]}');
+    final delimiterLexer = DelimiterLexerExtension();
+
+    expect(delimiterLexer.lex(scanner), DelimiterToken(Delimiter.openBrace));
+    expect(delimiterLexer.lex(scanner), DelimiterToken(Delimiter.openBracket));
+    expect(delimiterLexer.lex(scanner), DelimiterToken(Delimiter.doubleQuote));
+    expect(delimiterLexer.lex(scanner), DelimiterToken(Delimiter.colon));
+    expect(delimiterLexer.lex(scanner), DelimiterToken(Delimiter.comma));
+    expect(delimiterLexer.lex(scanner), DelimiterToken(Delimiter.closeBracket));
+    expect(delimiterLexer.lex(scanner), DelimiterToken(Delimiter.closeBrace));
+  });
+
+  test('should return null if scanned char is not delimiter', () {
+    var scanner = Scanner('te');
+
+    expect(DelimiterLexerExtension().lex(scanner), null);
+  });
+}
